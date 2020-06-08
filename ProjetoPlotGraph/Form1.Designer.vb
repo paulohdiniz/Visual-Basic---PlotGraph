55ca93f2fe9c0e1a386521b8d908e3bb5d9441a6
paulohdiniz <ph.diniz@usp.br>
2020-06-08 09:41:19 -0300

    plot graph in visual basic

---
+++ ProjetoPlotGraph/Form1.Designer.vb
@@ -0,0 +1,69 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Chart1
+        '
+        ChartArea1.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea1)
+        Legend1.Name = "Legend1"
+        Me.Chart1.Legends.Add(Legend1)
+        Me.Chart1.Location = New System.Drawing.Point(0, 0)
+        Me.Chart1.Name = "Chart1"
+        Me.Chart1.Size = New System.Drawing.Size(1500, 800)
+        Me.Chart1.TabIndex = 0
+        Me.Chart1.Text = "Chart1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(720, 806)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(150, 76)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "PLOT"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1782, 953)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Chart1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Chart1 As DataVisualization.Charting.Chart
+    Friend WithEvents Button1 As Button
+End Class
---
+++ ProjetoPlotGraph/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'setup the chart area 
+        Chart1.Titles.Add("Line of x^2") 'specify chart name
+        Chart1.ChartAreas.Clear()
+        Chart1.ChartAreas.Add("Default")
+        With Chart1.ChartAreas("Default")
+            .AxisX.Title = "X" 'x label
+            .AxisX.MajorGrid.LineColor = Color.SkyBlue
+            .AxisY.MajorGrid.LineColor = Color.SkyBlue
+            .AxisY.Title = "Y" 'y label
+        End With
+        'specify series plot lines
+        Chart1.Series.Clear()
+        Chart1.Series.Add("plot1")
+        Chart1.Series("plot1").Color = Color.Red
+        Chart1.Series("plot1").ChartType = DataVisualization.Charting.SeriesChartType.Line
+
+        Dim n As Integer = 1000 ' number of points
+        Dim y As Single
+        For x As Integer = 1 To n Step 1
+            y = x * x
+            Chart1.Series("plot1").Points.AddXY(x, y)
+        Next
+    End Sub
+End Class
